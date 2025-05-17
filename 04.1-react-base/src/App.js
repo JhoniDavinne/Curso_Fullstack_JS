857e7861069cc8493c97e08f4a72ba9dfd349b90
Jhoni Davinne <jhoni.davinne@hotmail.com>
2025-05-17 02:01:51 -0300

    Header da app

--- 04.1-react-base/src/App.js
+++ 04.1-react-base/src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 
 import Login from "./pages/Login";
 import GlobalStyles from "./styles/GlobalStyles";
+import Header from "./components/header";
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyles />
     </>
