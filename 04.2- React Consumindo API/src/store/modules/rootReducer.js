4736ce28663d9fc4802496c92fb6ae92c77d77b7
Jhoni Davinne <jhoni.davinne@hotmail.com>
2025-05-24 02:13:01 -0300

    Remove pasta 04.2- React Consumindo API (extraída para submódulo)

--- 04.2- React Consumindo API/src/store/modules/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import exampleReducer from "./examples/reducer";
-
-export default combineReducers({
-  example: exampleReducer,
-});
--- 04.2- React Consumindo API/src/store/modules/rootSaga.js
+++
@@ -1,7 +0,0 @@
-import { all } from "redux-saga/effects";
-
-import example from "./examples/sagas";
-
-export default function* rootSaga() {
-  yield all([example]);
-}
